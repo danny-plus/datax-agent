b4dd5c88dcfd20722eeb2019dae943f98ba41d42
danny_ni <me@danny-ni.com>
2020-09-01 13:59:16 +0800

    init

---
+++ src/main/java/ni/danny/dataxagent/DataxAgentApplication.java
@@ -0,0 +1,13 @@
+package ni.danny.dataxagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataxAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataxAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ni/danny/dataxagent/DataxAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.danny.dataxagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataxAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
