7e026b15bcf3c68eb495a85c85a7ac4e2f286268
danny_ni <me@danny-ni.com>
2020-09-07 19:31:48 +0800

    FIX:编译问题

--- src/main/java/ni/danny/dataxagent/controller/DataxAgentController.java
+++ src/main/java/ni/danny/dataxagent/controller/DataxAgentController.java
@@ -4,6 +4,7 @@ import com.alipay.common.tracer.core.context.trace.SofaTraceContext;
 import com.alipay.common.tracer.core.holder.SofaTraceContextHolder;
 import com.alipay.common.tracer.core.span.SofaTracerSpan;
 import lombok.extern.slf4j.Slf4j;
+import ni.danny.dataxagent.callback.ExecutorDataxJobCallback;
 import ni.danny.dataxagent.dto.ResponseDTO;
 import ni.danny.dataxagent.dto.resp.AnsycExcuteRespDTO;
 import ni.danny.dataxagent.enums.RespDTOEnum;
@@ -29,7 +30,12 @@ public class DataxAgentController {
     @GetMapping("/excuteJob")
     @ResponseBody
     public ResponseDTO excuteJob(@RequestParam String jobId,@RequestParam int taskId,@RequestParam String jobJsonPath) throws Throwable {
-        dataxAgentService.asyncExecuteDataxJob(jobId,new Random().nextInt(999),jobJsonPath);
+        dataxAgentService.asyncExecuteDataxJob(jobId, new Random().nextInt(999), jobJsonPath, new ExecutorDataxJobCallback() {
+            @Override
+            public void finishTask() {
+                log.info("finish task");
+            }
+        });
         SofaTraceContext sofaTraceContext = SofaTraceContextHolder.getSofaTraceContext();
         SofaTracerSpan sofaTracerSpan = sofaTraceContext.getCurrentSpan();
 
