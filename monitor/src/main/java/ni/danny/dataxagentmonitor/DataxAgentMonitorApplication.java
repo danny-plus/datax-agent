8fc631dd3e952b2374239a0e3bb0913dc476cb45
danny_ni <me@danny-ni.com>
2020-09-24 14:42:24 +0800

    next_feat:监控页面

---
+++ monitor/src/main/java/ni/danny/dataxagentmonitor/DataxAgentMonitorApplication.java
@@ -0,0 +1,13 @@
+package ni.danny.dataxagentmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataxAgentMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataxAgentMonitorApplication.class, args);
+	}
+
+}
